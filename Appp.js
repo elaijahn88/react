3f1600ee2401e0a2863f71305a545d3c2280abad
elaijahn88 <elajahn8@gmail.com>
2025-08-02 11:40:38 +0300

    Create Appp.js

---
+++ Appp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+      <h2>S media</h2>
+
+      <ReactPlayer
+        url="https://www.w3schools.com/html/mov_bbb.mp4"
+        controls={true}
+        width="100%"
+        height="450px"
+      />
+    </div>
+  );
+}
+
+export default App;
