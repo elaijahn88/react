7a9b0994c9f3a43901bab219ca0a2d0fc43e749c
elaijahn88 <elajahn8@gmail.com>
2025-08-02 11:34:45 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 function App() {
   return (
     <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
-      <h2>🎥 React Video Player Example</h2>
+      <h2>🎥 Silicon media</h2>
 
       <ReactPlayer
         url="https://www.w3schools.com/html/mov_bbb.mp4"
