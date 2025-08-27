90b9e939d001df32a420776a96ca056fe90e41e1
elaijahn88 <elajahn8@gmail.com>
2025-08-28 02:34:27 +0300

    Create Apppp.js

---
+++ Apppp.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import { Video } from "react-native-video";
+
+const { width } = Dimensions.get("window");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Video
+        source={{ uri: "https://www.w3schools.com/html/mov_bbb.mp4" }} // sample video
+        style={styles.video}
+        controls={true}
+        resizeMode="contain"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    width: width,
+    height: 250,
+  },
+});
