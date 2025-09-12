02e0544535119293b19d872774935b292515722d
Elijah <elajahn8@gmail.com>
2025-09-12 11:24:42 +0300

    Update Apppp.js

--- Apppp.js
+++ Apppp.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Video
-        source={{ uri: "https://www.w3schools.com/html/mov_bbb.mp4" }} // sample video
+        source={{ uri: "https://xlijah.com/soso.mp4" }} // sample video
         style={styles.video}
         controls={true}
         resizeMode="contain"
