b1a9b142c6be0c768e872be5e9e2104773ea6a48
Elijah <elajahn8@gmail.com>
2025-09-23 13:00:48 +0300

    Update and rename firebase.tsx to firebase.js

---
+++ app/firebase/src/firebase.js
@@ -0,0 +1,32 @@
+// Import Firebase SDK functions you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your config (from google-services.json + project settings)
+const firebaseConfig = {
+  apiKey: "AIzaSyC-tkURRvTk80HH-kfAvnW8V396iO1lcIE",
+  authDomain: "file-6f3ac.firebaseapp.com",
+  projectId: "file-6f3ac",
+  storageBucket: "file-6f3ac.firebasestorage.app",
+  messagingSenderId: "588974859374",
+  appId: "1:588974859374:android:b0661bd1bfdb356aa3f27f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export services for use in your app
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+
+
+
+
+
+
+
+
