d64decadbac19d0fe2d27764838eaddcbf4678bf
Elijah <elajahn8@gmail.com>
2025-09-29 13:18:56 +0300

    Update firebase.js

--- app/firebase/src/firebase.js
+++ app/firebase/src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getMessaging } from "firebase/messaging";
 
 // 🔑 Your Firebase config
 const firebaseConfig = {
