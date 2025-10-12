1e0f91ef7fea923b76fd3642143eec8a5e206a6c
Elijah <elajahn8@gmail.com>
2025-10-12 07:27:21 +0300

    Delete app/final/app_token.tsx

--- app/final/app_token.tsx
+++
@@ -1,7 +0,0 @@
-import messaging from "@react-native-firebase/messaging";
-
-async function getFcmToken() {
-  const token = await messaging().getToken();
-  console.log("FCM Registration Token:", token);
-  return token;
-}
